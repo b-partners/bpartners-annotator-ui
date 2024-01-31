1a0c3849fe3c99aa53cf695c26c1ef06602591d2
amourRamanantsiresy <hei.amour.2@gmail.com>
2024-01-31 09:38:12 +0300

    config: css file injection

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
+import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
-
+import { libInjectCss } from 'vite-plugin-lib-inject-css'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts({ include: ['lib'] })],
+  plugins: [react(), libInjectCss(), dts({ include: ['lib'] })],
   build: {
     lib: {
       entry: resolve(__dirname, 'lib/index.ts'),
