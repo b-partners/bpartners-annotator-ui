6f6630ff28d4efe19258591f6e56009e9d75dbc9
amourRamanantsiresy <hei.amour.2@gmail.com>
2024-02-02 11:59:33 +0300

    chore: implement use image creation

---
+++ lib/hooks/use-image-creation.ts
@@ -0,0 +1,13 @@
+import { useEffect, useState } from "react";
+
+export const useImageCreation = (src: string) => {
+  const [image, setImage] = useState<HTMLImageElement>(new Image());
+
+  useEffect(() => {
+    const img = new Image();
+    img.onload = () => setImage(img);
+    img.src = src;
+  }, [src]);
+
+  return image;
+};
