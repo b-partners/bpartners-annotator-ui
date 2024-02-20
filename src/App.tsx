60ca3b92221d9ac23619911e063b936c31367afe
amourRamanantsiresy <hei.amour.2@gmail.com>
2024-02-21 00:22:23 +0300

    chore: update app.tsx for test

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { useState } from 'react';
 import { AnnotatorCanvas } from '../lib';
 import image from '../src/assets/image.png';
+import { Polygon } from '../lib/types';
 
 function App() {
+  const [polygons, setPolygons] = useState<Polygon[]>([]);
+
   return (
     <div>
-      <AnnotatorCanvas height='70vh' width='60vw' addPolygone={() => {}} polygoneList={[]} image={image} />
+      <AnnotatorCanvas height='70vh' width='60vw' addPolygone={polygon => setPolygons(prev => [...prev, polygon])} polygoneList={polygons} image={image} />
     </div>
   );
 }
