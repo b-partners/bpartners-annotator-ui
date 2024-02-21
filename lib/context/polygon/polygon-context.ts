4c5a714a8218ae301f9a8f43381d9830b0071c8f
amourRamanantsiresy <hei.amour.2@gmail.com>
2024-02-21 14:01:55 +0300

    fix: types error

--- lib/context/polygon/polygon-context.ts
+++ lib/context/polygon/polygon-context.ts
@@ -1,9 +1,10 @@
 import { createContext } from 'react';
 import { PolygonContextType } from '.';
+import { defaultPolygon } from '../../constant';
 
 export const PolygonContext = createContext<PolygonContextType>({
   addPolygon: () => {},
   polygons: [],
   isDrawing: false,
-  polygon: null,
+  polygon: defaultPolygon,
 });
--- lib/hooks/use-draw-static-image.ts
+++ lib/hooks/use-draw-static-image.ts
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from 'react';
 import { useElementContext, useImagePosition, useSizesContext } from '.';
-import { CanvasHandler } from '..';
+import { CanvasHandler, ScaleHandler } from '..';
 
 export const useDrawStaticImage = () => {
   const { imageHeight, imageWidth } = useSizesContext();
@@ -9,8 +9,9 @@ export const useDrawStaticImage = () => {
   const { imageX, imageY } = useImagePosition();
 
   useEffect(() => {
-    if (image.src.length > 0 && canvasRef) {
-      const canvasImageHandler = new CanvasHandler(canvasRef.current as HTMLCanvasElement);
+    if (image.src.length > 0 && canvasRef && canvasRef.current) {
+      const scaleHandler = new ScaleHandler(canvasRef.current, image);
+      const canvasImageHandler = new CanvasHandler(canvasRef.current, scaleHandler);
       canvasImageHandler.drawImage(image, imageX, imageY, imageWidth, imageHeight);
     }
   }, [image, imageX, imageY, imageWidth, imageHeight, canvasRef]);
