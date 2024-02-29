109e281a74d22bf598ba178cd5e8e01b4269a444
amourRamanantsiresy <hei.amour.2@gmail.com>
2024-02-29 09:32:15 +0300

    fix: show polygon from polygon list render

--- lib/hooks/use-draw-polygons.ts
+++ lib/hooks/use-draw-polygons.ts
@@ -1,7 +1,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { RefObject, useEffect } from 'react';
 import { useElementContext, usePolygonContext, useSizesContext } from '.';
-import { ScaleHandler, CanvasHandler } from '..';
+import { ScaleHandler, CanvasHandler, UrlParams } from '..';
 
 export const useDrawPolygon = (cursorCanvasRef: RefObject<HTMLCanvasElement>, polygonCanvasRef: RefObject<HTMLCanvasElement>) => {
   const { scale } = useSizesContext();
@@ -10,6 +10,7 @@ export const useDrawPolygon = (cursorCanvasRef: RefObject<HTMLCanvasElement>, po
 
   useEffect(() => {
     if (cursorCanvasRef.current && polygonCanvasRef.current) {
+      UrlParams.set('scale', scale.toFixed(2));
       const cursorCanvas = cursorCanvasRef.current;
       const polygonCanvas = polygonCanvasRef.current;
       const scaleHandler = new ScaleHandler(cursorCanvas, image);
