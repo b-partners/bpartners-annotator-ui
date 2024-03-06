8af8868fb9613cd3374e566a0ddafae37fe6f1e3
amourRamanantsiresy <hei.amour.2@gmail.com>
2024-03-06 13:34:48 +0300

    chore: configure dotenv

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,16 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 import { libInjectCss } from 'vite-plugin-lib-inject-css';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), libInjectCss(), dts({ include: ['lib'], exclude: ['lib/__tests__'] })],
+  define: {
+    'process.env': process.env,
+  },
   build: {
     lib: {
       entry: resolve(__dirname, 'lib/index.ts'),
