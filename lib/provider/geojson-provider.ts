08fde0f33023d9836d27a7468f26ee5b57635f9a
amourRamanantsiresy <hei.amour.2@gmail.com>
2024-03-06 13:47:43 +0300

    chore: add converter provider

---
+++ lib/provider/geojson-provider.ts
@@ -0,0 +1,17 @@
+import { Geojson, GeojsonReturn } from '..';
+
+export const pointsToGeoPoints = async (body: Geojson) => {
+  try {
+    const res = await fetch(process.env.ANNOTATOR_GEO_CONVERTER_API_URL || '', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    });
+    return (await res.json()) as GeojsonReturn[];
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
