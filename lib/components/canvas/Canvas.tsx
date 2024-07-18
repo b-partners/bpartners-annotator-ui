59f84e03e4051a8052d706c378d981a8b1186bb6
RAMANANTSIRESY Amour Bien Aimé <95756837+AmourRamanantsiresy@users.noreply.github.com>
2024-07-18 16:06:08 +0300

    fix: draw polygon under measurements
    * fix: allow click under measurements
    
    * fix: draw polygon under measurements

--- lib/components/canvas/Canvas.tsx
+++ lib/components/canvas/Canvas.tsx
@@ -29,7 +29,7 @@ export const Canvas = () => {
 
   useEffect(() => {
     if (cursorCanvasRef.current) {
-      const sc = new ScaleHandler(cursorCanvasRef.current as HTMLCanvasElement, image);
+      const sc = new ScaleHandler(cursorCanvasRef.current, image);
       setInfo({
         physicalMeasurements: measurements.map(measurement => {
           const position = sc.getPhysicalPositionByPoint(measurement.position);
@@ -49,7 +49,7 @@ export const Canvas = () => {
         const { x: left, y: top } = position;
         return (
           unity === 'm' && (
-            <span key={k} className={style.measurement} style={{ top, left, fontSize: `${+(UrlParams.get('scale') || '1') * 5}px` }}>
+            <span key={`${value}-measure-${k}`} className={style.measurement} style={{ top, left, fontSize: `${+(UrlParams.get('scale') ?? '1') * 5.7}px` }}>
               {value}
               {unity}
             </span>
